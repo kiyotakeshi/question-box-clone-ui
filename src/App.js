e7281a53116800b47a2f1fe46820a8d65b51094a
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-04-14 02:47:54 +0900

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import axios from 'axios';
+import { For } from 'react-loops';
+
+//         "id": "ecefeaf1-9786-4110-a95a-78f5c9bb8755",
+// "questioner": "test3",
+// "post": "これはテスト投稿です",
+// "respondent": "回答者",
+// "answer": null,
+// "answered": false,
+// "archived": false
 
 function App() {
-  return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
-  );
+    let response = await axios.get('http://localhost:8080/list');
+    console.log(response);
+    return (
+        <div className="App">
+            {/* <table>
+                <tr>
+                    <th>ID</th>
+                    <th>questioner</th>
+                    <th>post</th>
+                    <th>respondent</th>
+                    <th>answer</th>
+                    <th>answered</th>
+                    <th>archived</th>
+                </tr>
+            </table> */}
+        </div>
+    );
 }
 
 export default App;
