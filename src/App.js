d4713c62857e60cdcc1b783fc4a707f8f5820afa
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-27 15:48:30 +0900

    functional component を使用した event の取得を実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react';
 import List from './component/question/list';
+import { QuestionContext } from './context/QuestionContext';
 
 const App = () => {
-      return (
-          <div>
-              <List/>
-          </div>
-      );
-}
+    return (
+        <div>
+            <QuestionContext.Provider value={{}}>
+                <List />
+            </QuestionContext.Provider>
+        </div>
+    );
+};
 export default App;
--- src/component/question/list.jsx
+++ src/component/question/list.jsx
@@ -1,50 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { For } from 'react-loops';
 import { Card } from 'semantic-ui-react';
 import Question from './question';
 import axios from 'axios';
 
-export default class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: [],
-            showingDetail: false,
-            detail: null
-        }
-    }
+const List = () => {
+    const initState = {
+        list: [],
+        showingDetail: false,
+        detail: null,
+    };
 
-    componentDidMount() {
-        axios.get(`/list`)
-            .then(res => {
-                this.setState({ list: res.data });
-            });
-    }
+    const [state, setState] = useState(initState);
+    const { list, showingDetail, detail } = state;
 
-    changeView = (key,e) => {
-        this.setState({
-            showingDetail: !this.state.showingDetail,
-            detail: this.state.list.filter(i => i.id === key).pop()
+    useEffect(() => {
+        axios.get(`/list`).then((res) => {
+            setState({ ...state, list: res.data });
         });
+    }, []);
 
-    }
-
-    render() {
-        return this.state.showingDetail ? (
-            <Question
-                item={ this.state.detail }
-                onClick={ this.changeView.bind(this) }
-            />
-        ) : (
+    return (
+        <>
             <Card.Group>
-                <For of={ this.state.list }>{ question => (
-                    <Question
-                        key={ question.id }
-                        item={ question }
-                        onClick={ this.changeView.bind(this, question.id) }
-                    />
-                ) }</For>
+                <For of={list}>
+                    {(question) => (
+                        <Question key={question.id} item={question} />
+                    )}
+                </For>
             </Card.Group>
-        );
-    }
-}
+        </>
+    );
+};
+
+export default List;
+
+// export default class List extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             list: [],
+//             showingDetail: false,
+//             detail: null
+//         }
+//     }
+
+//     componentDidMount() {
+//         axios.get(`/list`)
+//             .then(res => {
+//                 this.setState({ list: res.data });
+//             });
+//     }
+
+//     changeView = (key,e) => {
+//         this.setState({
+//             showingDetail: !this.state.showingDetail,
+//             detail: this.state.list.filter(i => i.id === key).pop()
+//         });
+
+//     }
+
+//     render() {
+//         return this.state.showingDetail ? (
+//             <Question
+//                 item={ this.state.detail }
+//                 onClick={ this.changeView.bind(this) }
+//             />
+//         ) : (
+//             <Card.Group>
+//                 <For of={ this.state.list }>{ question => (
+//                     <Question
+//                         key={ question.id }
+//                         item={ question }
+//                         onClick={ this.changeView.bind(this, question.id) }
+//                     />
+//                 ) }</For>
+//             </Card.Group>
+//         );
+//     }
+// }
