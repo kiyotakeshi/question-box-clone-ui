e9d5c0bcfb8d4cf0623892c049e3ec0200d77e9e
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-27 17:49:28 +0900

    query parameter をハードコーディングで追加

--- src/component/question/list.jsx
+++ src/component/question/list.jsx
@@ -1,56 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { For } from 'react-loops';
-import { Card } from 'semantic-ui-react';
-import Question from './question';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { For } from "react-loops";
+import { Card } from "semantic-ui-react";
+import Question from "./question";
+import axios from "axios";
 
 const List = () => {
-    const initState = {
-        list: [],
-        showingDetail: false,
-        detail: null,
-    };
+  const initState = {
+    list: [],
+    showingDetail: false,
+    detail: null,
+  };
 
-    const [state, setState] = useState(initState);
-    const { list, showingDetail, detail } = state;
+  const [state, setState] = useState(initState);
+  const { list, showingDetail, detail } = state;
 
-    useEffect(() => {
-        axios.get(`/list`).then((res) => {
-            setState({ ...state, list: res.data });
-        });
-    }, []);
+  useEffect(() => {
+    axios.get(`/list?answer=false&archived=false/`).then((res) => {
+      setState({ ...state, list: res.data });
+    });
+  }, []);
 
-    const changeView = (key) => {
-        setState({
-            ...state,
-            showingDetail: !showingDetail,
-            detail: list.filter((i) => i.id === key).pop(),
-        });
-    };
+  const changeView = (key) => {
+    setState({
+      ...state,
+      showingDetail: !showingDetail,
+      detail: list.filter((i) => i.id === key).pop(),
+    });
+  };
 
-    return (
-        <>
-            {showingDetail && (
-                <Question
-                    item={detail}
-                    onClick={changeView.bind(this, detail.id)}
-                />
+  return (
+    <>
+      {showingDetail && (
+        <Question item={detail} onClick={changeView.bind(this, detail.id)} />
+      )}
+      {!showingDetail && (
+        <Card.Group>
+          <For of={list}>
+            {(question) => (
+              <Question
+                key={question.id}
+                item={question}
+                onClick={changeView.bind(this, question.id)}
+              />
             )}
-            {!showingDetail && (
-                <Card.Group>
-                    <For of={list}>
-                        {(question) => (
-                            <Question
-                                key={question.id}
-                                item={question}
-                                onClick={changeView.bind(this, question.id)}
-                            />
-                        )}
-                    </For>
-                </Card.Group>
-            )}
-        </>
-    );
+          </For>
+        </Card.Group>
+      )}
+    </>
+  );
 };
 
 export default List;
